5a28f092f36348c46b91a9f23ea4e6d0f08fa3cc
grsagor <grsagor08@gmail.com>
2022-10-24 23:02:11 +0600

    firebase and bootstrap installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFhF-mRH9ygCESb_2AuFijH7WXbKOpmGY",
+  authDomain: "assignment-10-40242.firebaseapp.com",
+  projectId: "assignment-10-40242",
+  storageBucket: "assignment-10-40242.appspot.com",
+  messagingSenderId: "1068085253195",
+  appId: "1:1068085253195:web:c6272ed1bc68e72b90e9ce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
