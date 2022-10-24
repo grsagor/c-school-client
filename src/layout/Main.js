e6398624f0d4c9ebb3903af3fc6b31fbd6da23d5
grsagor <grsagor08@gmail.com>
2022-10-25 00:05:23 +0600

    route set up initially

---
+++ src/layout/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from '../Pages/Shared/Header/Header';
+import LeftSideNav from '../Pages/Shared/LestSideNav/LeftSideNav';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+            <Row>
+                <Col>
+                    <LeftSideNav></LeftSideNav>
+                </Col>
+                <Col>
+                    <Outlet></Outlet>
+                </Col>
+            </Row>
+        </Container>
+        </div>
+    );
+};
+
+export default Main;
